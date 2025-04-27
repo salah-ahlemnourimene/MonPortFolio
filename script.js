1b84cf70de88987ec29ecbfef9828e8bff764bd7
Ahlem <salahahlemnourimene@gmail.com>
2025-04-27 18:28:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Message simple dans la console
+console.log("Bienvenue sur mon Portfolio !");
+// Fonction pour afficher plus d'informations sur les projets
+const btnInfos = document.getElementById('btnInfos');
+const infosSupp = document.getElementById('infosSupp');
+
+btnInfos.addEventListener('click', () => {
+    if (infosSupp.style.display === "none") {
+        infosSupp.style.display = "block";
+        btnInfos.textContent = "Masquer les informations";
+    } else {
+        infosSupp.style.display = "none";
+        btnInfos.textContent = "Afficher plus d'informations";
+    }
+});
